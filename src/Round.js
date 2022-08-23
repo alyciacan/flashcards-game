ebd3cc6b9aed64093e5facce207a00d9c1a03c1d
Alycia Canavan <alycia.canavan@gmail.com>
2022-08-22 20:55:16 -0700

    Begin work on Round class

--- src/Round.js
+++ src/Round.js
@@ -1,4 +1,12 @@
-
+class Round {
+  constructor(deck) {
+    this.deck = deck.cards;
+  }
+  returnCurrentCard() {
+    console.log(this.deck[0])
+    return this.deck[0];
+  }
+}
 
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -5,3 +5,23 @@ const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
+
+describe('Round', function() {
+  it('should be a function', function() {
+    const round = new Round;
+
+    expect(Round).to.be.a('function');
+  });
+
+  it('should start out by playing the first card in the Deck', function() {
+    const card1 = new Card(1, 'How old is Alycia?', ['31', '30', '36', '35'], '36');
+    const card2 = new Card(2, 'In which state was Alycia born?', ['Maine', 'Utah', 'California', 'Rhode Island'], 'Maine');
+    const card3 = new Card(3, 'How many tattoos does Alycia have?', ['0', '3', '2', '7'], '3');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    expect(round.returnCurrentCard()).to.equal({ id: 1, question: 'How old is Alycia?', answers: ['31', '30', '36', '35'], correctAnswer: '36' } );
+  });
+
+
+});
