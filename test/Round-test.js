d8a07f1054cea34d73e3fbab2ff64e246746e553
Alycia Canavan <alycia.canavan@gmail.com>
2022-08-25 15:19:51 -0700

    Add beforeEach to clean up tests:

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,33 +7,43 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
 describe('Round', function() {
+  let card1;
+  let card2;
+  let card3;
+  let deck;
+  let round;
+  let card4;
+  let card5;
+  let card6;
+  let deck1;
+  let round1;
+
+  beforeEach(() => {
+    card1 = new Card(1, 'How old is Alycia?', ['31', '30', '36', '35'], '36');
+    card2 = new Card(2, 'In which state was Alycia born?', ['Maine', 'Utah', 'California', 'Rhode Island'], 'Maine');
+    card3 = new Card(3, 'How many tattoos does Alycia have?', ['0', '3', '2', '7'], '3');
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
+
+    card4 = new Card(1, 'How old is Alycia?', ['31', '30', '36', '35'], '36');
+    card5 = new Card(2, 'In which state was Alycia born?', ['Maine', 'Utah', 'California', 'Rhode Island'], 'Maine');
+    card6 = new Card(3, 'How many tattoos does Alycia have?', ['0', '3', '2', '7'], '3');
+    deck1 = new Deck([card4, card5, card6]);
+    round1 = new Round(deck1);
+  });
+
   it('should start out by playing the first card in the Deck', function() {
-    const card1 = new Card(1, 'How old is Alycia?', ['31', '30', '36', '35'], '36');
-    const card2 = new Card(2, 'In which state was Alycia born?', ['Maine', 'Utah', 'California', 'Rhode Island'], 'Maine');
-    const card3 = new Card(3, 'How many tattoos does Alycia have?', ['0', '3', '2', '7'], '3');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
 
     expect(round.returnCurrentCard()).to.equal(card1);
   });
 
   it('should start out with 0 turns and 0 incorrect guesses', function() {
-    const card1 = new Card(1, 'How old is Alycia?', ['31', '30', '36', '35'], '36');
-    const card2 = new Card(2, 'In which state was Alycia born?', ['Maine', 'Utah', 'California', 'Rhode Island'], 'Maine');
-    const card3 = new Card(3, 'How many tattoos does Alycia have?', ['0', '3', '2', '7'], '3');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
 
     expect(round.turns).to.equal(0);
     expect(round.incorrectGuesses).to.have.lengthOf(0);
   });
 
   it('should update the currentCardIndex each time a turn is taken', function() {
-    const card1 = new Card(1, 'How old is Alycia?', ['31', '30', '36', '35'], '36');
-    const card2 = new Card(2, 'In which state was Alycia born?', ['Maine', 'Utah', 'California', 'Rhode Island'], 'Maine');
-    const card3 = new Card(3, 'How many tattoos does Alycia have?', ['0', '3', '2', '7'], '3');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
 
     round.takeTurn('30');
 
@@ -41,11 +51,6 @@ describe('Round', function() {
   });
 
   it('should then play the next card in the array on the next turn', function() {
-    const card1 = new Card(1, 'How old is Alycia?', ['31', '30', '36', '35'], '36');
-    const card2 = new Card(2, 'In which state was Alycia born?', ['Maine', 'Utah', 'California', 'Rhode Island'], 'Maine');
-    const card3 = new Card(3, 'How many tattoos does Alycia have?', ['0', '3', '2', '7'], '3');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
 
     round.takeTurn('30');
 
@@ -53,11 +58,6 @@ describe('Round', function() {
   });
 
   it('should evaluate and record the guess', function() {
-    const card1 = new Card(1, 'How old is Alycia?', ['31', '30', '36', '35'], '36');
-    const card2 = new Card(2, 'In which state was Alycia born?', ['Maine', 'Utah', 'California', 'Rhode Island'], 'Maine');
-    const card3 = new Card(3, 'How many tattoos does Alycia have?', ['0', '3', '2', '7'], '3');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
 
     round.takeTurn('30');
 
@@ -65,11 +65,6 @@ describe('Round', function() {
   });
 
   it('should store the id of the incorrectly answered question in incorrectGuesses array', function() {
-    const card1 = new Card(1, 'How old is Alycia?', ['31', '30', '36', '35'], '36');
-    const card2 = new Card(2, 'In which state was Alycia born?', ['Maine', 'Utah', 'California', 'Rhode Island'], 'Maine');
-    const card3 = new Card(3, 'How many tattoos does Alycia have?', ['0', '3', '2', '7'], '3');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
 
     round.takeTurn('30');
 
@@ -77,21 +72,10 @@ describe('Round', function() {
   });
 
   it('should return feedback whether the guess is incorrect or correct', function() {
-    const card1 = new Card(1, 'How old is Alycia?', ['31', '30', '36', '35'], '36');
-    const card2 = new Card(2, 'In which state was Alycia born?', ['Maine', 'Utah', 'California', 'Rhode Island'], 'Maine');
-    const card3 = new Card(3, 'How many tattoos does Alycia have?', ['0', '3', '2', '7'], '3');
-    const deck = new Deck([card1, card2, card3]);
-    const round = new Round(deck);
 
     const response = round.takeTurn('30');
 
     expect(response).to.equal('Incorrect!');
-
-    const card4 = new Card(1, 'How old is Alycia?', ['31', '30', '36', '35'], '36');
-    const card5 = new Card(2, 'In which state was Alycia born?', ['Maine', 'Utah', 'California', 'Rhode Island'], 'Maine');
-    const card6 = new Card(3, 'How many tattoos does Alycia have?', ['0', '3', '2', '7'], '3');
-    const deck1 = new Deck([card4, card5, card6]);
-    const round1 = new Round(deck1);
 
     const response1 = round1.takeTurn('36');
 
@@ -99,11 +83,6 @@ describe('Round', function() {
   });
 
   it('should calculate and return the percentage of correct guesses, along with an end game message', function() {
-    const card4 = new Card(1, 'How old is Alycia?', ['31', '30', '36', '35'], '36');
-    const card5 = new Card(2, 'In which state was Alycia born?', ['Maine', 'Utah', 'California', 'Rhode Island'], 'Maine');
-    const card6 = new Card(3, 'How many tattoos does Alycia have?', ['0', '3', '2', '7'], '3');
-    const deck1 = new Deck([card4, card5, card6]);
-    const round1 = new Round(deck1);
 
     const response1 = round1.takeTurn('36');
     const response2 = round1.takeTurn('Maine');
